bceb0a39e46bf155673a04ff6742533605b8f4d3
Dhriti Peshkar <dhritipesh@gmail.com>
2025-07-29 11:43:31 -0700

    Added todolist

---
+++ MeridianApp/TodoViewMain.swift
@@ -0,0 +1,64 @@
+//
+//  TodoViewMain.swift
+//  MeridianApp
+//
+//  Created by Scholar on 7/29/25.
+//
+
+//
+//  ContentView.swift
+//  ToDoListApp
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+import SwiftData
+struct ContentView: View {
+  @State private var showNewTask = false
+  @Query var toDos: [ToDoItem]
+  var body: some View {
+    VStack {
+      HStack{
+        Text("To Do List")
+          .font(.system(size: 40))
+             .fontWeight(.black)
+        Spacer()
+        Button {
+          withAnimation {
+            showNewTask = true
+          }
+        } label: {
+          Text("+")
+            .font(.title)
+            .fontWeight(.bold)
+        }
+      }
+      .padding()
+      Spacer()
+      List {
+          ForEach (toDos) { toDoItem in
+              Text(toDoItem.title)
+          }
+      }
+    }
+    if showNewTask {
+      NewToDoView(toDoItem: ToDoItem(title: "", isImportant: false))
+    }
+  }
+}
+#Preview {
+  ContentView()
+}
+
+
+
+
+
+
+
+
+    
+#Preview {
+    ContentView()
+}
