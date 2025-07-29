6e97766570274d078e05a4678c6e861949f8a69d
Ella Ragoler <ella.ragoler@gmail.com>
2025-07-29 13:35:54 -0700

    Added fully functioning to-do list

--- MeridianApp/ToDoItem.swift
+++ MeridianApp/ToDoItem.swift
@@ -5,15 +5,17 @@
 //  Created by Scholar on 7/29/25.
 //
 
-//import Foundation
-//import SwiftData
-//@Model
-//class ToDoItem {
-//  var title: String
-//  var isImportant: Bool
-//  init(title: String, isImportant: Bool = false) {
-//    self.title = title
-//    self.isImportant = isImportant
-//  }
-//}
+import Foundation
+import SwiftData
+
+@Model
+class ToDoItem {
+    var title: String
+    var isImportant: Bool
+    
+    init(title: String, isImportant: Bool = false) {
+        self.title = title
+        self.isImportant = isImportant
+    }
+}
 
--- MeridianApp/ToDoList.swift
+++ MeridianApp/ToDoList.swift
@@ -5,15 +5,16 @@
 //  Created by Scholar on 7/29/25.
 //
 
-//import SwiftUI
-//import SwiftData
-//@main
-//struct ToDoListAppApp: App {
-//  var body: some Scene {
-//    WindowGroup {
-//      ContentView()
-//        .modelContainer(for: ToDoItem.self)
-//    }
-//  }
-//}
+import SwiftUI
+import SwiftData
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .modelContainer(for: ToDoItem.self)
+        }
+    }
+}
 
