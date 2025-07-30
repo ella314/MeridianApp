58cce73e2c458d74cb18720737dc4cdc94a8800c
Ella Ragoler <ella.ragoler@gmail.com>
2025-07-30 11:43:26 -0700

    Journal Password + Timer aesthetics

--- MeridianApp/Timer.swift
+++ MeridianApp/Timer.swift
@@ -13,67 +13,71 @@ struct TimerView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Timer")
-                .font(.system(size: 60))
-            Text(timeString(from: timeRemaining))
-                .font(.system(size: 48, weight: .bold, design: .monospaced))
-                .padding()
-                .background(.blue)
-                .cornerRadius(15)
-                .foregroundStyle(Color.white)
-                .shadow(radius:3)
-            
-            HStack(spacing: 20) {
-                Button(timerRunning ? "Pause" : "Start") {
-                    timerRunning.toggle()
-                }
-                .font(.title2)
-                .padding(8)
-                .background(.blue)
-                .foregroundStyle(Color.white)
-                .cornerRadius(15)
-                .shadow(radius: 3)
-
-                Button("Reset") {
-                    timeRemaining = 0
-                    timerRunning = false
-                }
-                .font(.title2)
-                .padding(8)
-                .background(.blue)
-                .foregroundStyle(Color.white)
-                .cornerRadius(15)
-                .shadow(radius: 3)
-            }
-            HStack {
-                Button("Study") {
-                    timeRemaining = 1500
-                }
-                .padding()
-                .frame(width: 80, height: 80)
-                .background(.blue)
-                .foregroundStyle(Color.white)
-                .cornerRadius(15)
-                .shadow(radius: 3)
-                Button("Short \nBreak") {
-                    timeRemaining = 300
-                }
-                .padding()
-                .frame(width: 80, height: 80)
-                .background(.blue)
+        ZStack{
+            Color("Linen")
+                .ignoresSafeArea()
+            VStack(spacing: 20) {
+                Text("Timer")
+                    .font(.system(size: 60))
+                Text(timeString(from: timeRemaining))
+                    .font(.system(size: 48, weight: .bold, design: .monospaced))
+                    .padding()
+                    .background(Color("Cambridge"))
                     .cornerRadius(15)
-                .foregroundStyle(Color.white)
-                .shadow(radius: 3)
-                Button("Long \nBreak") {
-                    timeRemaining = 600
+                    .foregroundStyle(Color.white)
+                    .shadow(radius:3)
+                
+                HStack(spacing: 20) {
+                    Button(timerRunning ? "Pause" : "Start") {
+                        timerRunning.toggle()
+                    }
+                    .font(.title2)
+                    .padding(8)
+                    .background(Color("Cambridge"))
+                    .foregroundStyle(Color.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 3)
+                    
+                    Button("Reset") {
+                        timeRemaining = 0
+                        timerRunning = false
+                    }
+                    .font(.title2)
+                    .padding(8)
+                    .background(Color("Cambridge"))
+                    .foregroundStyle(Color.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 3)
                 }
-                .padding()
-                .frame(width: 80, height: 80)
-                .background(.blue)
-                .foregroundStyle(Color.white)
-                .cornerRadius(15)
-                .shadow(radius: 3)
+                HStack {
+                    Button("Study") {
+                        timeRemaining = 1500
+                    }
+                    .padding()
+                    .frame(width: 80, height: 80)
+                    .background(Color("Cambridge"))
+                    .foregroundStyle(Color.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 3)
+                    Button("Short \nBreak") {
+                        timeRemaining = 300
+                    }
+                    .padding()
+                    .frame(width: 80, height: 80)
+                    .background(Color("Cambridge"))
+                    .cornerRadius(15)
+                    .foregroundStyle(Color.white)
+                    .shadow(radius: 3)
+                    Button("Long \nBreak") {
+                        timeRemaining = 600
+                    }
+                    .padding()
+                    .frame(width: 80, height: 80)
+                    .background(Color("Cambridge"))
+                    .foregroundStyle(Color.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 3)
+                }
             }
         }
         .onReceive(timer) { _ in
