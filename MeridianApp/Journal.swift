010270dc4aafd1a92ad61525ff3773faf7b1b19a
Dhriti Peshkar <dhritipesh@gmail.com>
2025-07-30 14:00:02 -0700

    hi

---
+++ MeridianApp/Journal.swift
@@ -0,0 +1,52 @@
+//
+//  Journal.swift
+//  MeridianApp
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct Journal: View {
+    var body: some View {
+        ZStack{
+           Color ("Linen")
+                .ignoresSafeArea()
+            VStack {
+                HStack{
+                    Text("                  Journal")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.black)
+                        .padding(.all, 5.0)
+                        Spacer()
+                    
+                    Button("+"){
+                       
+                    }
+                    .font(.largeTitle)
+                    .padding()
+
+                }//HStack
+                TextField("Journal here!", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/, axis: .vertical )
+<<<<<<< HEAD:Journal.swift
+                    .lineLimit(28...)
+=======
+                    .lineLimit(29...)
+>>>>>>> main:MeridianApp/Journal.swift
+                    .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 2)
+                        .padding(.all)
+              
+                
+                    Spacer()
+            }//VStack
+        }//ZStack
+    }//body
+}//struct
+
+#Preview {
+    Journal()
+}//preview
---
+++ MeridianApp/JournalPassword.swift
@@ -0,0 +1,59 @@
+//
+//  JournalPassword.swift
+//  MeridianApp
+//
+//  Created by Scholar on 7/30/25.
+//
+
+import SwiftUI
+
+struct JournalPasswordView: View {
+    @State private var password = ""
+    @State private var isAuthenticated = false
+    @State private var showError = false
+    
+    private let correctPassword = "hi"
+    
+    var body: some View {
+        NavigationStack {
+            ZStack{
+                Color ("Cambridge")
+                    .ignoresSafeArea()
+                VStack(spacing: 20) {
+                    Text("Enter Password")
+                        .font(.headline)
+                        .foregroundColor(Color("Linen 1"))
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                    Button("Submit") {
+                        if password == correctPassword {
+                            isAuthenticated = true
+                        } else {
+                            showError = true
+                            password = ""
+                        }
+                    }
+                    .padding()
+                    .background(Color.linen)
+                    .cornerRadius(15)
+                    .foregroundStyle(Color.black)
+                    if showError {
+                        Text("Incorrect password. Try again.")
+                            .foregroundColor(Color ("Linen"))
+                    }
+                }
+                .padding()
+                
+                NavigationLink("", destination: Journal(), isActive: $isAuthenticated)
+                    .hidden()
+            } //ZStack
+        }//NavStack
+            
+        }//Body
+    }//Struct
+
+
+#Preview {
+    JournalPasswordView()
+}
